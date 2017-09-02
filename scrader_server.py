92e8a37664c5f4cf0012e54c5703adfa99f6ae16
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2017-09-02 10:18:47 +0000

    chatfuel apis

--- scrader_server.py
+++ scrader_server.py
@@ -58,6 +58,216 @@ def get_latest_news():
                           status=status,
                           mimetype='application/json')
 
+@app.route('/news/<company>'.format(methods=['GET']))
+def get_news(company):
+    """ GET Server Status API endpoint
+        Args:
+        Returns:
+            dict: A JSON object containing the nfvacc server status information
+    """
+
+#
+#    response_data = {
+#  "messages": [
+#    {
+#      "attachment": {
+#        "type": "template",
+#        "payload": {
+#          "template_type": "button",
+#           "text": "Enjoy reading " + company +" news!",
+#          "buttons": [
+#            {
+#              "type": "web_url",
+#              "url": "http://money.cnn.com/2017/08/25/technology/business/amazon-whole-foods-strategy/index.html",
+#              "title": "What Amazon knows"
+#            },
+#            {
+#              "type": "web_url",
+#              "url": "http://money.cnn.com/2017/08/29/technology/india-right-to-privacy-tech-industry-aadhaar/index.html",
+#              "title": "Privacy is now a right in India"
+#            }
+#          ]
+#        }
+#      }
+#    }
+#  ]
+#}
+    response_data = {
+ "messages": [
+    {
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type": "list",
+          "top_element_style": "compact",
+          "elements":[
+            {
+              "title":"cnn.com",
+              "image_url":"http://i2.cdn.turner.com/money/dam/assets/170825082748-whole-foods-fresh-fruit-apples-1024x576.jpg",
+
+              "buttons":[
+                {
+                  "type": "web_url",
+                  "url": "http://money.cnn.com/2017/08/25/technology/business/amazon-whole-foods-strategy/index.html",
+                  "title": "What Amazon knows"
+                }
+              ]
+            },
+            {
+              "title":"bloomberg.com",
+              "image_url": "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i4GOoR9HbhHg/v0/1000x-1.jpg",
+              "buttons":[
+                {
+              "type": "web_url",
+              "url": "https://www.bloomberg.com/news/articles/2017-08-29/ocado-taps-amazon-s-alexa-for-voice-ordering-in-convenience-push",
+              "title": "Ocado Taps Amazon's Alexa"
+            }
+              ]
+            }
+             ]
+          }
+         }
+        }
+       ]
+      }
+
+    status = 200 if response_data is not None else 403
+    js = json.dumps(response_data, indent=2)
+    return flask.Response(js,
+                          status=status,
+                          mimetype='application/json')
+
+
+@app.route('/top_stocks'.format(methods=['GET']))
+def get_top_stocks():
+    """ GET Server Status API endpoint
+        Args:
+        Returns:
+            dict: A JSON object containing the nfvacc server status information
+    """
+    response_data = {
+ 	"messages": [
+   		{"text": "Nokia"},
+  		{"text": "Erricson"},
+                {"text": "Amazon"}
+	 ]
+    }
+
+    response_data = {
+ "messages": [
+    {
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type": "list",
+          "top_element_style": "compact",
+          "elements":[
+            {
+              "title":"Amazon",
+              "image_url":"https://www.wired.com/wp-content/uploads/2016/04/amazon-a-logo-1200x630.jpg",
+            
+              "buttons":[
+                {
+              	  "type": "show_block",
+              	  "block_name": "Amazon_news",
+              	  "title": "Check out the news!"
+                }#,
+#                {
+#                  "type":"web_url",
+#                  "url":"https://www.amazon.com/",
+#                  "title":"Enter amazon.com"
+#                }  
+              ]
+            },
+            {
+              "title":"Apple",
+              "image_url": "https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201703170823",
+              "buttons":[
+                {
+                  "type": "show_block",
+                  "block_name": "Apple_news",
+                  "title": "Check out the news!"
+                }
+#                {
+#                  "type":"web_url",
+#                  "url":"https://www.apple.com/",
+#                  "title":"Enter apple.com"
+#                }
+              ]
+            },
+         {
+              "title":"Adidas",
+              "image_url": "https://mymall.vn/static/images/2014/12/02/1417490727_adidas.jpg",
+              "buttons":[
+                {
+                  "type": "show_block",
+                  "block_name": "Addidas_news",
+                  "title": "Check out the news!"
+                }
+#                {
+#                  "type":"web_url",
+#                  "url":"https://www.adidas.com/",
+#                  "title":"Enter adidas.com"
+#                }
+              ]
+            }
+
+
+	     ]
+          }
+         }
+        }
+       ]
+      }
+    status = 200 if response_data is not None else 403
+    js = json.dumps(response_data, indent=2)
+    return flask.Response(js,
+                          status=status,
+                          mimetype='application/json')
+
+@app.route('/top_stocks_new'.format(methods=['GET']))
+def get_top_stocks_new():
+    """ GET Server Status API endpoint
+        Args:
+        Returns:
+            dict: A JSON object containing the nfvacc server status information
+    """
+    response_data = {
+  "messages": [
+    {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+           "text": "Great choice! These are today's top positive stocks",
+          "buttons": [
+            {
+                  "type": "show_block",
+                  "block_name": "Amazon_news",
+                  "title": "Amazon"
+            },
+            {
+                  "type": "show_block",
+                  "block_name": "Apple_news",
+                  "title": "Apple"
+            },
+            {
+                  "type": "show_block",
+                  "block_name": "Addidas_news",
+                  "title": "Adidas"
+            }
+          ]
+        }
+      }
+    }
+  ]
+} 
+    status = 200 if response_data is not None else 403
+    js = json.dumps(response_data, indent=2)
+    return flask.Response(js,
+                          status=status,
+                          mimetype='application/json')
+
 
 def signal_sigint_handler(rec_signal, rec_frame):
     os._exit(1)
@@ -68,6 +278,7 @@ if __name__ == '__main__':
 
     # set the flask logger => ERROR: do not print API calls
     import logging
+
 
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.ERROR)
