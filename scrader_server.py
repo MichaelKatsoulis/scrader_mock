fd6fe9d91ed0389ce0957953619e05c24b159934
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2017-09-02 13:40:56 +0300

    a

--- scrader_server.py
+++ scrader_server.py
@@ -58,6 +58,7 @@ def get_latest_news():
                           status=status,
                           mimetype='application/json')
 
+
 @app.route('/news/<company>'.format(methods=['GET']))
 def get_news(company):
     """ GET Server Status API endpoint
@@ -66,70 +67,94 @@ def get_news(company):
             dict: A JSON object containing the nfvacc server status information
     """
 
-#
-#    response_data = {
-#  "messages": [
-#    {
-#      "attachment": {
-#        "type": "template",
-#        "payload": {
-#          "template_type": "button",
-#           "text": "Enjoy reading " + company +" news!",
-#          "buttons": [
-#            {
-#              "type": "web_url",
-#              "url": "http://money.cnn.com/2017/08/25/technology/business/amazon-whole-foods-strategy/index.html",
-#              "title": "What Amazon knows"
-#            },
-#            {
-#              "type": "web_url",
-#              "url": "http://money.cnn.com/2017/08/29/technology/india-right-to-privacy-tech-industry-aadhaar/index.html",
-#              "title": "Privacy is now a right in India"
-#            }
-#          ]
-#        }
-#      }
-#    }
-#  ]
-#}
-    response_data = {
- "messages": [
-    {
-      "attachment":{
-        "type":"template",
-        "payload":{
-          "template_type": "list",
-          "top_element_style": "compact",
-          "elements":[
-            {
-              "title":"cnn.com",
-              "image_url":"http://i2.cdn.turner.com/money/dam/assets/170825082748-whole-foods-fresh-fruit-apples-1024x576.jpg",
+    # response_data = {
+    #     "messages": [
+    #         {
+    #             "attachment": {
+    #                 "type": "template",
+    #                 "payload": {
+    #                     "template_type": "list",
+    #                     "top_element_style": "compact",
+    #                     "elements": [
+    #                         {
+    #                             "title": "cnn.com",
+    #                             "image_url":
+    #                                 "http://i2.cdn.turner.com/money/dam/assets/170825082748-whole-foods-fresh-fruit-apples-1024x576.jpg",
+    #
+    #                             "buttons": [
+    #                                 {
+    #                                   "type": "web_url",
+    #                                   "url": "http://money.cnn.com/2017/08/25/technology/business/amazon-whole-foods-strategy/index.html",
+    #                                   "title": "What Amazon knows"
+    #                                 }
+    #                             ]
+    #                         },
+    #                         {
+    #                             "title": "bloomberg.com",
+    #                             "image_url": "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i4GOoR9HbhHg/v0/1000x-1.jpg",
+    #                             "buttons": [
+    #                                 {
+    #                                     "type": "web_url",
+    #                                     "url": "https://www.bloomberg.com/news/articles/2017-08-29/ocado-taps-amazon-s-alexa-for-voice-ordering-in-convenience-push",
+    #                                     "title": "Ocado Taps Amazon's Alexa"
+    #                                 }
+    #                             ]
+    #                         }
+    #                     ]
+    #                 }
+    #             }
+    #         }
+    #     ]
+    # }
 
-              "buttons":[
-                {
-                  "type": "web_url",
-                  "url": "http://money.cnn.com/2017/08/25/technology/business/amazon-whole-foods-strategy/index.html",
-                  "title": "What Amazon knows"
-                }
-              ]
-            },
+    response_data = {
+         "messages": [
             {
-              "title":"bloomberg.com",
-              "image_url": "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i4GOoR9HbhHg/v0/1000x-1.jpg",
-              "buttons":[
-                {
-              "type": "web_url",
-              "url": "https://www.bloomberg.com/news/articles/2017-08-29/ocado-taps-amazon-s-alexa-for-voice-ordering-in-convenience-push",
-              "title": "Ocado Taps Amazon's Alexa"
+              "attachment":{
+                "type":"template",
+                "payload":{
+                  "template_type":"generic",
+                  "elements":[
+                    {
+                      "title":"Classic White T-Shirt",
+                      "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
+                      "subtitle":"Soft white cotton t-shirt is back in style",
+                      "buttons":[
+                        {
+                          "type":"web_url",
+                          "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
+                          "title":"View Item"
+                        },
+                        {
+                          "type":"web_url",
+                          "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
+                          "title":"Buy Item"
+                        }
+                      ]
+                    },
+                    {
+                      "title":"Classic Grey T-Shirt",
+                      "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
+                      "subtitle":"Soft gray cotton t-shirt is back in style",
+                      "buttons":[
+                        {
+                          "type":"web_url",
+                          "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
+                          "title":"View Item"
+                        },
+                        {
+                          "type":"web_url",
+                          "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
+                          "title":"Buy Item"
+                        }
+                      ]
+                    }
+                  ]
+                }
+              }
             }
-              ]
-            }
-             ]
-          }
-         }
+          ]
         }
-       ]
-      }
 
     status = 200 if response_data is not None else 403
     js = json.dumps(response_data, indent=2)
@@ -145,13 +170,6 @@ def get_top_stocks():
         Returns:
             dict: A JSON object containing the nfvacc server status information
     """
-    response_data = {
- 	"messages": [
-   		{"text": "Nokia"},
-  		{"text": "Erricson"},
-                {"text": "Amazon"}
-	 ]
-    }
 
     response_data = {
  "messages": [
@@ -168,52 +186,35 @@ def get_top_stocks():
             
               "buttons":[
                 {
-              	  "type": "show_block",
-              	  "block_name": "Amazon_news",
-              	  "title": "Check out the news!"
-                }#,
-#                {
-#                  "type":"web_url",
-#                  "url":"https://www.amazon.com/",
-#                  "title":"Enter amazon.com"
-#                }  
+                    "type": "show_block",
+                    "block_name": "Amazon_news",
+                    "title": "Check out the news!"
+                }
               ]
             },
             {
               "title":"Apple",
               "image_url": "https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201703170823",
-              "buttons":[
+              "buttons": [
                 {
                   "type": "show_block",
                   "block_name": "Apple_news",
                   "title": "Check out the news!"
                 }
-#                {
-#                  "type":"web_url",
-#                  "url":"https://www.apple.com/",
-#                  "title":"Enter apple.com"
-#                }
               ]
             },
          {
               "title":"Adidas",
               "image_url": "https://mymall.vn/static/images/2014/12/02/1417490727_adidas.jpg",
-              "buttons":[
+              "buttons": [
                 {
                   "type": "show_block",
                   "block_name": "Addidas_news",
                   "title": "Check out the news!"
                 }
-#                {
-#                  "type":"web_url",
-#                  "url":"https://www.adidas.com/",
-#                  "title":"Enter adidas.com"
-#                }
               ]
             }
-
-
-	     ]
+          ]
           }
          }
         }
@@ -225,6 +226,7 @@ def get_top_stocks():
                           status=status,
                           mimetype='application/json')
 
+
 @app.route('/top_stocks_new'.format(methods=['GET']))
 def get_top_stocks_new():
     """ GET Server Status API endpoint
@@ -233,35 +235,35 @@ def get_top_stocks_new():
             dict: A JSON object containing the nfvacc server status information
     """
     response_data = {
-  "messages": [
-    {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "button",
-           "text": "Great choice! These are today's top positive stocks",
-          "buttons": [
+        "messages": [
             {
-                  "type": "show_block",
-                  "block_name": "Amazon_news",
-                  "title": "Amazon"
-            },
-            {
-                  "type": "show_block",
-                  "block_name": "Apple_news",
-                  "title": "Apple"
-            },
-            {
-                  "type": "show_block",
-                  "block_name": "Addidas_news",
-                  "title": "Adidas"
+              "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "button",
+                    "text": "Great choice! These are today's top positive stocks",
+                    "buttons": [
+                        {
+                              "type": "show_block",
+                              "block_name": "Amazon_news",
+                              "title": "Amazon"
+                        },
+                        {
+                              "type": "show_block",
+                              "block_name": "Apple_news",
+                              "title": "Apple"
+                        },
+                        {
+                              "type": "show_block",
+                              "block_name": "Addidas_news",
+                              "title": "Adidas"
+                        }
+                    ]
+                }
+              }
             }
-          ]
-        }
-      }
+        ]
     }
-  ]
-} 
     status = 200 if response_data is not None else 403
     js = json.dumps(response_data, indent=2)
     return flask.Response(js,
