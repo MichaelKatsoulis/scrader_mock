2469b6d9c9110d3fa32563607d8c0c8b6accae5a
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2017-09-03 12:44:24 +0300

    a

--- scrader_server.py
+++ scrader_server.py
@@ -332,11 +332,6 @@ def get_companies(stocks_type):
                                     "type": "show_block",
                                     "block_name": "Company News",
                                     "title": "IBM"
-                                },
-                                {
-                                    "type": "show_block",
-                                    "block_name": "Company News",
-                                    "title": "Amazon"
                                 }
                             ]
                         }
