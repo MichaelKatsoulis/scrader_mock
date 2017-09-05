dd0f91aac126e418c5f189d4152e2da2d2baf128
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2017-09-05 22:04:32 +0300

    a

--- scrader_server.py
+++ scrader_server.py
@@ -59,14 +59,15 @@ def get_latest_news():
                           mimetype='application/json')
 
 
-@app.route('/subscribe/<user_id>'.format(methods=['POST']))
-def subscribe(user_id):
+@app.route('/subscribe/<user_id>/<time_frame>'.format(methods=['POST']))
+def subscribe(user_id, time_frame):
     """ GET Server Status API endpoint
         Args:
         Returns:
             dict: A JSON object containing the nfvacc server status information
     """
     print(user_id)
+    print(time_frame)
     response_data = {}
     status = 200 if response_data is not None else 403
     js = json.dumps(response_data, indent=2)
