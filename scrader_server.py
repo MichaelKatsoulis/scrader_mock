ae6386e89ed2f37d22bc8cb75c4e15a07f5aa49e
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2017-09-12 19:35:36 +0300

    a

--- scrader_server.py
+++ scrader_server.py
@@ -169,7 +169,7 @@ def subscribe(user_id, user_name):
         Returns:
             dict: A JSON object containing the nfvacc server status information
     """
-    data = flask.request.get_json()
+    data = request.data
     print(data)
     user_dict = {
             'user_id': str(user_id),
