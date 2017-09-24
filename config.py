cbee525ae564f86850b6c5fcc056a0902b1ffa12
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2017-09-24 17:37:04 +0300

    a

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 HOST = "127.0.0.1"
 PORT = 8000
 companies = ['Amazon', 'Google', 'Microsoft', 'Ericsson', 'Apple', 'Nokia']
-sources = ['cnn', 'twitter', 'news24']
+sources = ['cnn', 'twitter', 'nesws24']
