976d9f4e838e015e1d806e0bd26c0d3d0ed5bd56
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2017-09-24 21:18:55 +0300

    a

--- scrader_server.py
+++ scrader_server.py
@@ -148,7 +148,7 @@ def company_typed_search(company_typed):
     for company_type, companies_list in all_companies.items():
         for company_dict in companies_list:
             company_name = company_dict.get('company_name')
-            if company_typed in company_name.lower()
+            if company_typed in company_name.lower():
                 company_found = company_dict.get('company_name')
                 return company_found
 
