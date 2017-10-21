112bc0f116ebce9c593bca417accddd2a0601498
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2017-10-21 14:00:28 +0300

    users mongo

--- utils.py
+++ utils.py
@@ -127,7 +127,7 @@ def news_poll(poll_time):
 
 def add_article(article):
     import hashlib
-    id = int(hashlib.md5(article.get('image_url')))
+    id = hashlib.md5(article.get('image_url'))
     new_articles.articles[id] = article
 
 
